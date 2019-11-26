5fe859bbfe9285d35af08566fcc935ec2a9984bf
rayxxsuzuya <56367629+rayxxsuzuya@users.noreply.github.com>
2019-11-26 21:22:09 +0300

    hover css + js animations

--- js/script.js
+++ js/script.js
@@ -64,3 +64,129 @@ document.querySelectorAll('.page-services .container .flex-item')[3].onmouseout 
 
 	document.querySelectorAll('h3')[3].style.color = '#555555';
 };
+
+
+
+document.querySelectorAll('.page-projects .container .grid-box .grid-box-item')[0].onmouseover = function gridHover() {
+	document.querySelectorAll('.page-projects .container .grid-box .grid-box-item div h3')[0].style.color = '#ffffff';
+	document.querySelectorAll('.page-projects .container .grid-box .grid-box-item div')[0].style.backgroundColor = '#362f2d';
+	document.querySelectorAll('.page-projects .container .grid-box .grid-box-item div p')[0].style.color = '#c7b299';
+};
+
+document.querySelectorAll('.page-projects .container .grid-box .grid-box-item')[0].onmouseout = function gridHover() {
+	document.querySelectorAll('.page-projects .container .grid-box .grid-box-item div h3')[0].style.color = '#c7b299';
+	document.querySelectorAll('.page-projects .container .grid-box .grid-box-item div')[0].style.backgroundColor = '#fbfaf8';
+	document.querySelectorAll('.page-projects .container .grid-box .grid-box-item div p')[0].style.color = '#d1d1d1';
+};
+
+document.querySelectorAll('.page-projects .container .grid-box .grid-box-item')[1].onmouseover = function gridHover() {
+	document.querySelectorAll('.page-projects .container .grid-box .grid-box-item div h3')[1].style.color = '#ffffff';
+	document.querySelectorAll('.page-projects .container .grid-box .grid-box-item div')[1].style.backgroundColor = '#362f2d';
+	document.querySelectorAll('.page-projects .container .grid-box .grid-box-item div p')[1].style.color = '#c7b299';
+};
+
+document.querySelectorAll('.page-projects .container .grid-box .grid-box-item')[1].onmouseout = function gridHover() {
+	document.querySelectorAll('.page-projects .container .grid-box .grid-box-item div h3')[1].style.color = '#c7b299';
+	document.querySelectorAll('.page-projects .container .grid-box .grid-box-item div')[1].style.backgroundColor = '#fbfaf8';
+	document.querySelectorAll('.page-projects .container .grid-box .grid-box-item div p')[1].style.color = '#d1d1d1';
+};
+
+document.querySelectorAll('.page-projects .container .grid-box .grid-box-item')[2].onmouseover = function gridHover() {
+	document.querySelectorAll('.page-projects .container .grid-box .grid-box-item div h3')[2].style.color = '#ffffff';
+	document.querySelectorAll('.page-projects .container .grid-box .grid-box-item div')[2].style.backgroundColor = '#362f2d';
+	document.querySelectorAll('.page-projects .container .grid-box .grid-box-item div p')[2].style.color = '#c7b299';
+};
+
+document.querySelectorAll('.page-projects .container .grid-box .grid-box-item')[2].onmouseout = function gridHover() {
+	document.querySelectorAll('.page-projects .container .grid-box .grid-box-item div h3')[2].style.color = '#c7b299';
+	document.querySelectorAll('.page-projects .container .grid-box .grid-box-item div')[2].style.backgroundColor = '#fbfaf8';
+	document.querySelectorAll('.page-projects .container .grid-box .grid-box-item div p')[2].style.color = '#d1d1d1';
+};
+
+document.querySelectorAll('.page-projects .container .grid-box .grid-box-item')[3].onmouseover = function gridHover() {
+	document.querySelectorAll('.page-projects .container .grid-box .grid-box-item div h3')[3].style.color = '#ffffff';
+	document.querySelectorAll('.page-projects .container .grid-box .grid-box-item div')[3].style.backgroundColor = '#362f2d';
+	document.querySelectorAll('.page-projects .container .grid-box .grid-box-item div p')[3].style.color = '#c7b299';
+};
+
+document.querySelectorAll('.page-projects .container .grid-box .grid-box-item')[3].onmouseout = function gridHover() {
+	document.querySelectorAll('.page-projects .container .grid-box .grid-box-item div h3')[3].style.color = '#c7b299';
+	document.querySelectorAll('.page-projects .container .grid-box .grid-box-item div')[3].style.backgroundColor = '#fbfaf8';
+	document.querySelectorAll('.page-projects .container .grid-box .grid-box-item div p')[3].style.color = '#d1d1d1';
+};
+
+document.querySelectorAll('.page-projects .container .grid-box .grid-box-item')[4].onmouseover = function gridHover() {
+	document.querySelectorAll('.page-projects .container .grid-box .grid-box-item div h3')[4].style.color = '#ffffff';
+	document.querySelectorAll('.page-projects .container .grid-box .grid-box-item div')[4].style.backgroundColor = '#362f2d';
+	document.querySelectorAll('.page-projects .container .grid-box .grid-box-item div p')[4].style.color = '#c7b299';
+};
+
+document.querySelectorAll('.page-projects .container .grid-box .grid-box-item')[4].onmouseout = function gridHover() {
+	document.querySelectorAll('.page-projects .container .grid-box .grid-box-item div h3')[4].style.color = '#c7b299';
+	document.querySelectorAll('.page-projects .container .grid-box .grid-box-item div')[4].style.backgroundColor = '#fbfaf8';
+	document.querySelectorAll('.page-projects .container .grid-box .grid-box-item div p')[4].style.color = '#d1d1d1';
+};
+
+document.querySelectorAll('.page-projects .container .grid-box .grid-box-item')[5].onmouseover = function gridHover() {
+	document.querySelectorAll('.page-projects .container .grid-box .grid-box-item div h3')[5].style.color = '#ffffff';
+	document.querySelectorAll('.page-projects .container .grid-box .grid-box-item div')[5].style.backgroundColor = '#362f2d';
+	document.querySelectorAll('.page-projects .container .grid-box .grid-box-item div p')[5].style.color = '#c7b299';
+};
+
+document.querySelectorAll('.page-projects .container .grid-box .grid-box-item')[5].onmouseout = function gridHover() {
+	document.querySelectorAll('.page-projects .container .grid-box .grid-box-item div h3')[5].style.color = '#c7b299';
+	document.querySelectorAll('.page-projects .container .grid-box .grid-box-item div')[5].style.backgroundColor = '#fbfaf8';
+	document.querySelectorAll('.page-projects .container .grid-box .grid-box-item div p')[5].style.color = '#d1d1d1';
+};
+
+document.querySelectorAll('.page-posts .container .flex-posts .flex-posts-item')[0].onmouseover = function postsHover() {
+	document.querySelectorAll('.page-posts .container .flex-posts .flex-posts-item div + div')[0].style.backgroundColor = '#362f2d';
+	document.querySelectorAll('.page-posts .container .flex-posts .flex-posts-item div + div h3')[0].style.color = '#c7b299';
+	document.querySelectorAll('.page-posts .container .flex-posts .flex-posts-item div + div p')[0].style.color = '#ffffff';
+};
+
+document.querySelectorAll('.page-posts .container .flex-posts .flex-posts-item')[0].onmouseout = function postsHover() {
+	document.querySelectorAll('.page-posts .container .flex-posts .flex-posts-item div + div')[0].style.backgroundColor = '#ffffff';
+	document.querySelectorAll('.page-posts .container .flex-posts .flex-posts-item div + div h3')[0].style.color = '#555555';
+	document.querySelectorAll('.page-posts .container .flex-posts .flex-posts-item div + div p')[0].style.color = '#8c8c8c';
+};
+
+document.querySelectorAll('.page-posts .container .flex-posts .flex-posts-item')[1].onmouseover = function postsHover() {
+	document.querySelectorAll('.page-posts .container .flex-posts .flex-posts-item div + div')[1].style.backgroundColor = '#362f2d';
+	document.querySelectorAll('.page-posts .container .flex-posts .flex-posts-item div + div h3')[1].style.color = '#c7b299';
+	document.querySelectorAll('.page-posts .container .flex-posts .flex-posts-item div + div p')[1].style.color = '#ffffff';
+};
+
+document.querySelectorAll('.page-posts .container .flex-posts .flex-posts-item')[1].onmouseout = function postsHover() {
+	document.querySelectorAll('.page-posts .container .flex-posts .flex-posts-item div + div')[1].style.backgroundColor = '#ffffff';
+	document.querySelectorAll('.page-posts .container .flex-posts .flex-posts-item div + div h3')[1].style.color = '#555555';
+	document.querySelectorAll('.page-posts .container .flex-posts .flex-posts-item div + div p')[1].style.color = '#8c8c8c';
+};
+
+document.querySelectorAll('.page-posts .container .flex-posts .flex-posts-item')[2].onmouseover = function postsHover() {
+	document.querySelectorAll('.page-posts .container .flex-posts .flex-posts-item div + div')[2].style.backgroundColor = '#362f2d';
+	document.querySelectorAll('.page-posts .container .flex-posts .flex-posts-item div + div h3')[2].style.color = '#c7b299';
+	document.querySelectorAll('.page-posts .container .flex-posts .flex-posts-item div + div p')[2].style.color = '#ffffff';
+};
+
+document.querySelectorAll('.page-posts .container .flex-posts .flex-posts-item')[2].onmouseout = function postsHover() {
+	document.querySelectorAll('.page-posts .container .flex-posts .flex-posts-item div + div')[2].style.backgroundColor = '#ffffff';
+	document.querySelectorAll('.page-posts .container .flex-posts .flex-posts-item div + div h3')[2].style.color = '#555555';
+	document.querySelectorAll('.page-posts .container .flex-posts .flex-posts-item div + div p')[2].style.color = '#8c8c8c';
+};
+
+document.querySelector('.flex-buttons .left-btn img').onmouseover = function buttonHover () {
+	this.src = 'img/posts-left-hover.png';
+};
+
+document.querySelector('.flex-buttons .left-btn img').onmouseout = function buttonHover () {
+	this.src = 'img/posts-left.png';
+};
+
+document.querySelector('.flex-buttons .right-btn img').onmouseover = function buttonHover () {
+	this.src = 'img/posts-right-hover.png';
+};
+
+document.querySelector('.flex-buttons .right-btn img').onmouseout = function buttonHover () {
+	this.src = 'img/posts-right.png';
+};
